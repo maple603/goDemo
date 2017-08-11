3e1ad1fd2fdda1d362e284baf24f1866da52a553
maple <jinsemuyefeng@gmail.com>
2017-08-12 00:41:50 +0800

    update

---
+++ Spider/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func print(c chan bool, n int) {
+	x := 0
+	for i := 1; i < 10000000; i++ {
+		x += 1
+	}
+	fmt.Println(n, x)
+	c <- true
+}
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	c := make(chan bool, 10)
+	for i := 0; i < 10; i++ {
+		go print(c, i)
+	}
+
+	for i := 0; i < 10; i++ {
+		<-c
+	}
+
+	fmt.Println("Done.")
+}
