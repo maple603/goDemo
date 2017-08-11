3a99b5940165729469b50827a67d93241c05d95f
maple <jinsemuyefeng@gmail.com>
2017-08-12 00:51:55 +0800

    update

--- Spider/main.go
+++ Spider/main.go
@@ -3,27 +3,39 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
-func print(c chan bool, n int) {
+func print(wg *sync.WaitGroup, n int) {
 	x := 0
 	for i := 1; i < 10000000; i++ {
 		x += 1
 	}
 	fmt.Println(n, x)
-	c <- true
+	//c <- true
+	wg.Done()
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	c := make(chan bool, 10)
-	for i := 0; i < 10; i++ {
-		go print(c, i)
-	}
+
+	// 管道方式
+	//c := make(chan bool, 10)
+
+	//
+	wg := sync.WaitGroup{}
+	//设置任务数
+	wg.Add(10)
 
 	for i := 0; i < 10; i++ {
-		<-c
+		go print(&wg, i)
 	}
+
+	//for i := 0; i < 10; i++ {
+	//	<-c
+	//}
+	//等待任务完成
+	wg.Wait()
 
 	fmt.Println("Done.")
 }
